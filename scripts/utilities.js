f79a7594eb6fd506bb1e21eeb919ab9418c203f4
Quellick <Quellick@gmail.com>
2017-08-14 15:52:04 -0700

    add updates from checkpoint instructions

---
+++ scripts/utilities.js
@@ -0,0 +1,29 @@
+var forEach = function (array, callback) {
+  for (var i = 0; i < array.length; i++){
+    callback();
+  }
+  function callback(){
+    animatePoints();
+  }
+}
+
+
+
+
+/*
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
+       var points = document.getElementsByClassName('point');
+
+       var revealPoint = function(index) {
+          points[index].style.opacity= 1;
+          points[index].style.transform = "scaleX(1) translateY(0)";
+          points[index].style.msTransform = "scaleX(1) translateY(0)";
+          points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
+    for  (var i = 0; i < points.length; i++){
+         revealPoint(i);
+    }
+};
+*/
