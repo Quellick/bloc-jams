bec5c9467f2821f166ca5e9589dc7aef987a1138
Quellick <Quellick@gmail.com>
2017-08-14 11:34:21 -0700

    change landing.js

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,20 +1,31 @@
 var pointsArray = document.getElementsByClassName('point');
+
 var animatePoints = function(points) {
+       var points = document.getElementsByClassName('point');
+
        var revealPoint = function(index) {
-          points[index].style.opacity= 1;points[1]
+          points[index].style.opacity= 1;
           points[index].style.transform = "scaleX(1) translateY(0)";
           points[index].style.msTransform = "scaleX(1) translateY(0)";
           points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
     }
-    for  (var i=0; i<=points.length; i++){
+    for  (var i = 0; i < points.length; i++){
          revealPoint(i);
     }
 };
 
 window.onload = function() {
-  var sellingPoints = document.getElementsByClassName('seeling-points')[0];
+  if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance - sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
   window.addEventListener('scroll', function(event) {
-     console.log("Current offset from the top is " + seelingPoints.getBoundingClientRect().top + " pixels");
+     console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+     if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);
+      }
  });
  }
