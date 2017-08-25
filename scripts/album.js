f48906d623ee2755cf129da4a3091b83dd35d047
Quellick <Quellick@gmail.com>
2017-08-25 11:41:32 -0700

    checkpoint-18-JQuery
    Complete,  I think.. is my mouseleave should replace the numbber not the pause icon though?

--- scripts/album.js
+++ scripts/album.js
@@ -48,7 +48,7 @@
        // Revert to song number for currently playing song because user started playing new song.
        var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
        currentlyPlayingCell.html(currentlyPlayingSong);
- }
+     }
        if (currentlyPlayingSong !== songNumber) {
        // Switch from Play -> Pause button to indicate new song is playing.
        $(this).html(pauseButtonTemplate);
@@ -57,7 +57,7 @@
        // Switch from Pause -> Play button to pause currently playing song.
        $(this).html(playButtonTemplate);
        currentlyPlayingSong = null;
- }
+     }
 };
 
      var onHover = function(event){
