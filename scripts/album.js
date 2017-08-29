e5ee1a4e13a44ceaf22e4297f3cb1740980325ac
Quellick <Quellick@gmail.com>
2017-08-29 10:53:15 -0700

    checkpoint-19
    committ more changes

--- scripts/album.js
+++ scripts/album.js
@@ -13,29 +13,26 @@
        var songNumber = parseInt($(this).attr('data-song-number'));
 
        if (currentlyPlayingSongNumber !== null) {
-       // Revert to song number for currently playing song because user started playing new song.
        var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
        currentlyPlayingCell.html(currentlyPlayingSongNumber);
      }
        if (currentlyPlayingSongNumber !== songNumber) {
-       // Switch from Play -> Pause button to indicate new song is playing.
        $(this).html(pauseButtonTemplate);
        currentlyPlayingSongNumber = songNumber;
        currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
        updatePlayerBarSong();
      } else if (currentlyPlayingSongNumber === songNumber) {
-       // Switch from Pause -> Play button to pause currently playing song.
        $(this).html(playButtonTemplate);
        $('.main-controls .play-pause').html(playerBarPlayButton);
        currentlyPlayingSongNumber = null;
        currentSongFromAlbum = null;
      }
-};
+    };
 
      var onHover = function(event){
        var $songNumberCell = $(this).find(".song-item-number")
        var songNumber = parseInt($(this).attr('data-song-number'));
-       //check to see if song is playing
+
        if(songNumber !== currentlyPlayingSongNumber){
          $songNumberCell.html(playButtonTemplate);
        }
@@ -44,6 +41,7 @@
      var offHover = function(event){
        var $songNumberCell = $(this).find(".song-item-number")
        var songNumber = parseInt($(this).attr('data-song-number'));
+
        if(songNumber !== currentlyPlayingSongNumber){
          $songNumberCell.html(songNumber);
          console.log("songNumber type is " + typeof songNumber + "\n and currentlyPlayingSongNumber type is " + typeof currentlyPlayingSongNumber);
@@ -63,16 +61,13 @@
      var $albumImage = $('.album-cover-art');
      var $albumSongList = $('.album-view-song-list');
 
-     // #2
      $albumTitle.text(album.title);
      $albumArtist.text(album.artist);
      $albumReleaseInfo.text(album.year + ' ' + album.label);
      $albumImage.attr('src', album.albumArtUrl);
 
-     // #3
      $albumSongList.empty();
 
-     // #4
      for (var i = 0; i < album.songs.length; i++) {
          var $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
          $albumSongList.append($newRow);
