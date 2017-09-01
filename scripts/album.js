ba724ca5d36cd92eec8df1b253a0d88f401678bf
Quellick <Quellick@gmail.com>
2017-09-01 12:43:24 -0700

    featurebranch-21-Jquery-seek-bar
    update to variables

--- scripts/album.js
+++ scripts/album.js
@@ -15,26 +15,27 @@ var setSong = function(songNumber){
 
   var setCurrentTimeInPlayerBar = function(currentTime){
     //set text of element with .current-time class to current time in the song
-    document.querySelector('.current-time').text(currentTime)
+    document.querySelector('.current-time').text(currentTime);
     //add method so current time updates with the song playback
-    updateSeekBarWhileSongPlays()
+    updateSeekBarWhileSongPlays();
   }
 
   var setTotalTimeInPlayerBar = function(totalTime){
     //set text of element with .total-time class to the length of the song
-    document.querySelector('.total-time').text(totalTime.length)
+    document.querySelector('.total-time').text(totalTime.length);
     //add method so the total time is set when a song first plays.
     updatePlayerBarSong();
   }
 
   var filterTimeCode = function(timeInSeconds){
     //Use parseFloat method to get the seconds in number form
-    parseFloat(timeInSeconds);
+    var value = parseFloat(timeInSeconds);
     //store variable for the whole seconds and whole minutes
-    var wholeSeconds = Math.floor()
-    var wholeMinutes = Math.floor()
+    var wholeSeconds = Math.floor('value');
+    //divide timeInSeconds by 60 to get whole minutes?
+    var wholeMinutes = Math.floor60('value');
     //return the time formatted as X:XX
-    return timeInSeconds
+    return (value[0] + ":" + value[1] + value[2]);
   }
 
   var seek = function(time) {
